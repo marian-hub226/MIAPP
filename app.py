76f572c3673df377cec6df6b2e86c26c282e22f1
marian-hub226 <marian.julianetas@gmail.com>
2026-02-26 07:07:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 import streamlit as st
+from PIL import Image
 
-st.title("MI PRIMERA PÁGINA CON STREAMLIT")
+st.title("Mi primera App!!")
+
+st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
+sr.write("Facilmente puedo realizar backend y frontend.")
+image = Image.open('Primera.jpg')

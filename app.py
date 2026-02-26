4a80a3fa79b4086475f01761b4cc3ac0e9c40322
marian-hub226 <marian.julianetas@gmail.com>
2026-02-26 07:14:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,7 @@ st.write("Facilmente puedo realizar backend y frontend.")
 image = Image.open('Primera.jpg')
 
 st.image(image, caption='Interfaces Multimodales')
+
+
+texto = st.text_input('Escribe algo', 'Este es mi texto')
+st.write('El texto escrito es', texto)

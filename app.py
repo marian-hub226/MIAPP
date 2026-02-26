147cd6e36a988223014486a3db9ee369ddcf238c
marian-hub226 <marian.julianetas@gmail.com>
2026-02-26 07:07:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ from PIL import Image
 st.title("Mi primera App!!")
 
 st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
-sr.write("Facilmente puedo realizar backend y frontend.")
+st.write("Facilmente puedo realizar backend y frontend.")
 image = Image.open('Primera.jpg')
